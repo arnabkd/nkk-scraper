f0052f9d939cc9e4b9ee96e422b9afc62a36a24f
Arnab Kumar Datta <arnab.datta@evry.com>
2020-05-09 02:15:03 +0200

    quick refactor

--- src/main/kotlin/nkk/scraper/App.kt
+++ src/main/kotlin/nkk/scraper/App.kt
@@ -56,30 +56,6 @@ fun printBreederTable(url: String) {
 
 }
 
-data class Breeder(
-  val fylke: Fylke? = null,
-  val name: String? = null,
-  val kennelName: String? = null,
-  val address: String? = null,
-  val phone: String? = null,
-  val email: String? = null,
-  val url: String? = null
-)
-
-enum class Fylke(val fullName: String) {
-  AGDER("Agder"),
-  INNLANDET("Innlandet"),
-  MORE_OG_ROMSDAL("Møre og Romsdal"),
-  NORDLAND("Nordland"),
-  OSLO("Oslo"),
-  ROGALAND("Rogaland"),
-  TROMS_OG_FINNMARK("Troms og Finmark"),
-  TRONDELAG("Trøndelag"),
-  VESTFOLD_OG_TELEMARK("Vestfold og Telemark"),
-  VESTLAND("Vestland"),
-  VIKEN("Viken")
-}
-
 fun main() {
   printBreederTable(goldenURL)
 }
