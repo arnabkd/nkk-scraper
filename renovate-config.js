9ff63269eb4d906cd54a3e82282ade78d9ed6469
Arnab Datta <arnab.datta@evry.com>
2020-07-15 21:07:49 +0200

    Update renovate-config.js

--- renovate-config.js
+++ renovate-config.js
@@ -8,34 +8,11 @@ module.exports =
   "extends": [
     "config:base"
   ],
-  // Renovate will be timing out against our nexus server anyway, so no need to look it up
-  "hostRules": [
-    {
-      "hostName": "https://nexus.tffauto.no/repository/maven-releases/",
-      "enabled": false
-    },
-    {
-      "hostName": "https://nexus.tffauto.no/repository/maven-snapshots/",
-      "enabled": false
-    }
-  ],
   // Ensure that major is never automerged
   "major": {
     "automerge": false
   },
   "masterIssue": true,
-  "packageRules": [
-    {
-      // We need to update tffauto-common manually as Renovate doesn't have access to nexus
-      "packageNames": ["tffauto-common"],
-      "enabled": false
-    },
-    {
-      // We don't need to update npm packages in this repo as they are just for dev purposes
-      "datasources": ["npm"],
-      "enabled": false
-    }
-  ],
   // Default 0, but use this to prevent renovate from spamming PRs
   "prConcurrentLimit": 5,
   // Wait for builds to complete (successful or otherwise) before creating PRs
